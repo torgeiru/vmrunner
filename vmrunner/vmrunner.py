d994f7c88ee44aac4db503eed996dba800e81819
torgeiru <torgeir.skovholt.pro@gmail.com>
2025-11-16 17:21:31 +0100

    Debug

--- vmrunner/vmrunner.py
+++ vmrunner/vmrunner.py
@@ -529,8 +529,8 @@ class qemu(hypervisor):
 
     def init_virtiofs(self, socket, shared, mem):
         """ initializes virtiofs by launching virtiofsd and creating a virtiofs device """
-        virtiofsd_args = ["virtiofsd", "--socket", socket, "--shared-dir", shared, "--sandbox", "none"]
-        self._virtiofsd_proc = subprocess.Popen(virtiofsd_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # pylint: disable=consider-using-with
+        virtiofsd_args = ["virtiofsd", "--log-level", "debug", "--socket", socket, "--shared-dir", shared, "--sandbox", "none"]
+        self._virtiofsd_proc = subprocess.Popen(virtiofsd_args) # pylint: disable=consider-using-with
 
         if self._virtiofsd_proc.poll():
             raise Exception("VirtioFSD failed to start")
