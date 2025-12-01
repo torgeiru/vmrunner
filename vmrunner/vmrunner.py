5534142fa67615307c1716b02a6f84ede58f67dd
torgeiru <torgeir.skovholt.pro@gmail.com>
2025-12-01 14:25:23 +0100

    Environment

--- vmrunner/vmrunner.py
+++ vmrunner/vmrunner.py
@@ -529,8 +529,11 @@ class qemu(hypervisor):
 
     def init_virtiofs(self, socket, shared, mem):
         """ initializes virtiofs by launching virtiofsd and creating a virtiofs device """
-        virtiofsd_args = ["RUST_BACKTRACE=1", "virtiofsd", "--log-level", "debug", "--socket", socket, "--shared-dir", shared, "--sandbox", "none"]
-        self._virtiofsd_proc = subprocess.Popen(virtiofsd_args) # pylint: disable=consider-using-with
+        env = os.environ.copy()
+        env['RUST_BACKTRACE'] = '1'
+
+        virtiofsd_args = ["virtiofsd", "--log-level", "debug", "--socket", socket, "--shared-dir", shared, "--sandbox", "none"]
+        self._virtiofsd_proc = subprocess.Popen(virtiofsd_args, env=env) # pylint: disable=consider-using-with
 
         if self._virtiofsd_proc.poll():
             raise Exception("VirtioFSD failed to start")
